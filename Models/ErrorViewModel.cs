942566af3f28a48575557d9df83172efb0e6eb70
Jeremy Camacho <jeremy.y.camacho@accenture.com>
2025-11-24 10:34:29 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevOpsWebApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
